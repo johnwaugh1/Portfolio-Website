4ed96119b9dab144be798fe88afa4ae72dd52360
johnwaugh1 <waughjm@appstate.edu>
2024-05-17 15:23:04 -0400

    finished updating project panel descriptions

--- projects-page/projects.js
+++ projects-page/projects.js
@@ -35,15 +35,34 @@ function openPanel(id) {
             break;
         case 'database':
             panelTitle.innerText = "Database System";
-            panelDescription.innerText = "Detailed description of Database System...";
+            panelDescription.innerText = "This project was completed in a course I took this previous semester, where our small team"
+            + " was tasked with coming up with a fictional company to design a database management system for, and then implementing this system"
+            + " in MySQL. Apart from pair programming, which boosted my knowledge of SQL, I assumed the role of project manager,"
+            + " helping to keep my team motivated and on track, reminding them of deadlines and assuring code quality."
+            + " Overall, this helped to familiarize me with SQL, and also build up my knowledge of key data management topics/resources"
+            + " like conceptual/relational models, schema design, and query/procedure/trigger design."
+            + "\n\n\ This project allowed me to find a new and interesting topic to explore, and I plan on using the"
+            + " skills I learned to implement a similar system in a future project dealing with big data.";
+            panelLink.innerHTML = '<a href="https://github.com/johnwaugh1/CS3430-Database-System" target=_blank>View this project on GitHub: https://github.com/johnwaugh1/CS3430-Database-System</a>';
             break;
         case 'statemachine':
             panelTitle.innerText = "State Machine";
-            panelDescription.innerText = "Detailed description of State Machine...";
+            panelDescription.innerText = "This was a project that was completed but not updated because our team wanted to take a different "
+            + "approach to the final product, but it still served as a learning experience for me to brush up on programming in C, and also understanding "
+            + "how to code for a specific microcontroller with many functionalities. The main purpose of this was really a test to see if the microcontroller "
+            + "would display the correct LED combination corresponding to a vehicles fault state when reading in CAN bus data."
+            + "\n\n Overall, I gained valuable skills in utilizing specialized IDE's for specific microcontrollers, coding with a larger team while sharing a "
+            + "centralized workspace, and how to convert data into a human readable format.";
+            panelLink.innerHTML = '<a href="https://github.com/johnwaugh1/ROSE-State-Machine" target=_blank>View this project on GitHub: https://github.com/johnwaugh1/ROSE-State-Machine</a>';
             break;
         case 'texteditor':
             panelTitle.innerText = "Text Editor";
-            panelDescription.innerText = "Detailed description of Text Editor...";
+            panelDescription.innerText = "This simple web-based text editor was html/css/js project I did after taking a course on web development, "
+            + "which got me interesting in the topic. It's purpose was to serve as an alternative or emulation of a text editor like Google Docs or Word, "
+            + "but it is very simplistic, and was more an exploration into the programming languages and what I could achieve with them when first starting out. "
+            + "\n\n I plan on using the same functions in this text editor in another project of mine, tranforming it into more of a note taking and saving feature "
+            + "within another web-based app I am developing. In addition, there are some improvements that I could implement down the line noted in the repository.";
+            panelLink.innerHTML = '<a href="https://github.com/johnwaugh1/Simple-Text-Editor" target=_blank>View this project on GitHub: https://github.com/johnwaugh1/Simple-Text-Editor</a>';
             break;
         default:
             break;
